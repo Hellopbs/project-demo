aed636b98586506138590cbd6b48edc8afa86077
unknown <153763699@qq.com>
2022-09-18 22:11:46 +0800

    banner

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="nav-bar">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style>
+  .nav-bar {
+    height: 44px;
+    line-height: 44px;
+    display: flex;
+    text-align: center;
+    background-color: var(--color-tint);
+    box-shadow: 0 3px 10px rgba(100, 100, 100, 0.5);
+  }
+  .left,.right{
+    width: 60px;
+  }
+  .center{
+    flex:1;
+    color:white;
+    
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,19 +1,46 @@
 <template>
-  <h1>这是HOME</h1>
+  <nav-bar>
+    <template #center>
+      <span >购物街</span>
+    </template>
+  </nav-bar>
+  <Swiper>
+    <SwiperItem v-for="item in banners">
+      <a :href="item.link">
+        <img :src="item.image"/>
+      </a>
+    </SwiperItem>
+  </Swiper>
 </template>
 
 <script>
+import NavBar from '@/components/common/navbar/NavBar.vue'
+import {getHomeMultiData} from '@/network/home.js'
+import {Swiper,SwiperItem} from '@/common/swiper/index'
+
 export default {
   name: 'HOME',
   data() {
     return {
-    };
+      banners:null,
+      recommend:null
+    }
   },
-  components: {},
+  components: {
+    NavBar,
+    Swiper,
+    SwiperItem
+},
   props: {},
   computed: {},
   methods: {},
-  created() {},
+  created() {
+    getHomeMultiData().then(res=>{
+      
+      this.banners=res.data.banner.list;
+      this.recommend=res.data.recommend.list
+    })
+  },
   mounted() {}
 };
 </script>
